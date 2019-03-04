7a47d2734e157111b204307fe5978369dc88be3e
Arpit Agrawal <arpita@cybage.com>
2019-03-04 14:56:52 +0530

    This is the first commit for this Demo Repository

---
+++ src/main/java/TestMaven/DemoMavenProject/App.java
@@ -0,0 +1,14 @@
+package TestMaven.DemoMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!!!!!!!!!" );
+    
+    }
+}

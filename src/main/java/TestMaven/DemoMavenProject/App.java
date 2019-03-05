5078fb2a8833a4ec623ae3b08ee0f0ddd566fda2
arpita <arpita@ArpitA.cybage.com>
2019-03-05 14:17:41 +0530

    second commit for the day

--- src/main/java/TestMaven/DemoMavenProject/App.java
+++ src/main/java/TestMaven/DemoMavenProject/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!!!!!!!!!!!" );
             System.out.println( "Hello World22!!!!!!!!!!!" );
             System.out.println( "Hello Anand" );
+            System.out.println( "Hello Anand how are you" );
 
     }
 }
